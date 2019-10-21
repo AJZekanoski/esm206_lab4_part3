5032bdfe8dee566f04ef3717117e02906548aed3
AJZekanoski <azekanoski@gmail.edu>
2019-10-21 13:47:26 -0700

    cleaning disease burden dataframe or whatever

---
+++ part3_AZ.R
@@ -0,0 +1,11 @@
+# AJ
+
+library(tidyverse)
+library(janitor)
+library(here)
+
+
+# Read in the data:
+db <- read_csv(here::here("data", "disease_burden.csv")) %>% 
+  clean_names() %>% 
+  rename(deaths_per_100k = death_rate_per_100_000)
